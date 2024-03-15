4d0d9bf92a6f16a92cc9681aa2f742d29b39ebc6
chaNcharge <chancharge@gmail.com>
2024-03-14 18:43:56 -0700

    Change Talapas code to match normal one

--- talapas/imagerecogtest.py
+++ talapas/imagerecogtest.py
@@ -1,4 +1,5 @@
 import torch
+import sys
 import torchvision.transforms as transforms
 from PIL import Image
 from imagerecogtrain import Net
@@ -11,7 +12,6 @@ transform = transforms.Compose([
 ])
 
 # Load the saved state dictionary
-#state_dict = torch.load('./cifar_net.pth', map_location=torch.device('cpu'))
 state_dict = torch.load('./cifar_net.pth')
 # Instantiate your model (assuming you're using the same architecture as before)
 model = Net()
@@ -25,10 +25,10 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
 
 # Function to predict the class of an input image
-def predict_image(image_path, model, transform):
+def predict_image(image_path, model, transform_fxn):
     # Open and preprocess the input image
     image = Image.open(image_path)
-    image = transform(image).unsqueeze(0)  # Add batch dimension
+    image = transform_fxn(image).unsqueeze(0)  # Add batch dimension
 
     # Perform inference
     with torch.no_grad():
@@ -42,7 +42,7 @@ def predict_image(image_path, model, transform):
 
 if __name__ == "__main__":
     # Path to the input image
-    image_path = './bird.jpg'  # Change this to the path of your image
+    image_path = sys.argv[1]  # Change this to the path of your image
 
     # Predict the class of the input image
     predicted_class = predict_image(image_path, model, transform)
