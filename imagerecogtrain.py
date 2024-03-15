80949412180ebe9ef5ec330620cb03fc968ef039
chaNcharge <chancharge@gmail.com>
2024-03-14 18:20:59 -0700

    Reformat some comments

--- imagerecogtrain.py
+++ imagerecogtrain.py
@@ -6,25 +6,18 @@ import torch.nn as nn
 import torch.optim as optim
 
 # Define the neural network model with Batch Normalization
-
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # Increase number of channels
-        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)  # Increase number of channels
         self.bn1 = nn.BatchNorm2d(32)                # Add Batch Normalization
-        # Increase number of channels
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1) # Increase number of channels
         self.bn2 = nn.BatchNorm2d(64)                # Add Batch Normalization
         self.pool = nn.MaxPool2d(2, 2)
-        # Increase number of channels
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1) # Increase number of channels
         self.bn3 = nn.BatchNorm2d(128)                # Add Batch Normalization
-        # Increase number of units
-        self.fc1 = nn.Linear(128 * 4 * 4, 512)
-        # Decrease number of units
-        self.fc2 = nn.Linear(512, 256)
+        self.fc1 = nn.Linear(128 * 4 * 4, 512)        # Increase number of units
+        self.fc2 = nn.Linear(512, 256)                # Decrease number of units
         self.fc3 = nn.Linear(256, 10)
 
     def forward(self, x):
